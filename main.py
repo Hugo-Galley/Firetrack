a232806e424209c371792e09e5f4dea104d5accd
Freaks <99678890+Hugo-Galley@users.noreply.github.com>
2022-10-27 21:34:04 +0200

    up

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from tkinter import *
 import playlist_fonction
+import webbrowser
 
-def ouverture_process_classe():
+def ouverture_process():
      playlist_fonction.add_user(enter_user.get())
      windows.destroy()
      playlist_fonction.lecteur_musqiue()
+
+def open_github_page():
+     webbrowser.open_new('https://github.com/Hugo-Galley/Firetrack')
 
 
 windows = Tk()
@@ -14,19 +18,20 @@ windows.geometry("480x360")
 windows.minsize(480,360)
 windows.config(background='#24A7A7')
 
-frame = Frame(windows,bg='#24A7A7')
+principal = Frame(windows,bg='#24A7A7')
+a_prpos = Frame(windows,bg='#24A7A7')
 
-En_tete = Label(frame, text="Bienvenue sur FireTrack",bg='#24A7A7',fg='white',font=("Courrier",25)).pack()
-nom = Label(frame, text="Username",bg='#24A7A7',fg='white',font=("Courier",13)).pack()
-enter_user = Entry(frame)
+En_tete = Label(principal, text="Bienvenue sur FireTrack",bg='#24A7A7',fg='white',font=("Courrier",25)).pack()
+nom = Label(principal, text="Username",bg='#24A7A7',fg='white',font=("Courier",13)).pack()
+enter_user = Entry(principal)
 enter_user.pack()
-classe = Label(frame, text="Classe",bg='#24A7A7',fg='white',font=("Courier",13)).pack()
-enter_class = Entry(frame)
-enter_class.pack()
-add_classe_button = Button(frame,text="Add",font=("Courier",17),bg='#24A7A7',fg='white',command=ouverture_process_classe).pack()
+add_user = Button(principal, text='Add', bg='#24A7A7',fg='white',font=("Courier",15),command=ouverture_process).pack()
+page_github = Button(a_prpos, text="Page Github",bg='#24A7A7',fg='white',font=("Courrier",14),command=open_github_page).pack()
 
-frame.pack()
 
+
+principal.pack()
+a_prpos.pack(side=BOTTOM)
 
 
 
