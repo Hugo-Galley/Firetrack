6c2268dd7430edff81a5dd3df6f7ecf6f332529c
Hugo-Galley <galleyhugo@icloud.com>
2022-12-09 13:50:37 +0100

    push

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-import tkinter
+from tkinter import *
+from customtkinter import *
+def create_room():
 
-ouvrir_fichier  = tkinter.filedialog.askopenfilename(title="Ouvrir un fichier",defaultextension=".mp3",filetypes=[("txt fichier",".mp3")])
+    win_room = CTk()
+    win_room.geometry("480x360")
+    win_room.minsize(480, 360)
+    win_room.config(background='#24A7A7')
+
+    CTkLabel(win_room, text='Crée une Room', bg_color='#24A7A7', fg_color='#24A7A7', font=('Courrier', 25)).pack()
+    CTkLabel(win_room, text='Username', bg_color='#24A7A7', fg_color='#24A7A7', font=('Courrier', 15)).pack()
+    enter_user_room = CTkEntry(win_room)
+    enter_user_room.pack()
+
+    CTkButton(win_room, text='Add', bg_color='#24A7A7', fg_color='#24A7A7', font=('Courier', 13),).pack()
+
+    win_room.mainloop()
+create_room()
