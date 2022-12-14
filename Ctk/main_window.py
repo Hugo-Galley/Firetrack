58f61aa2ee82e8d93502fdc15434e790d43349be
TheShadowGhost745 <hmagnier@yahoo.com>
2022-12-14 12:57:59 +0100

    up

--- Ctk/main_window.py
+++ Ctk/main_window.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 import customtkinter
 import pygame
 
@@ -20,3 +22,42 @@ class AdminMainWindow(window.Window):
         self.room.add_user(user)
         self.database.add_room(self.room)
         self.database.add_user(self.user)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.main_frame = MainFrame(master=self)
+        self.main_frame.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+    def open_menu(self):
+        pass
+
+
+class MainFrame(customtkinter.CTkFrame):
+
+    def __init__(self, *args, **kwargs):
+        super(MainFrame, self).__init__(*args, **kwargs)
+
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.song_label = customtkinter.CTkLabel(master=self, text="Current song name", font=("Courrier", 20))
+        self.song_label.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+        self.menu_button_image = customtkinter.CTkImage(light_image=Image.open("../assets/menu.png"),
+                                                        dark_image=Image.open("../assets/menu-modified.png"),
+                                                        size=(50, 50))
+
+        self.menu_button = customtkinter.CTkButton(master=self, image=self.menu_button_image, text="",
+                                                   command=self.master.open_menu)
+        self.menu_button.grid(row=0, column=0, padx=20, pady=20, sticky="nw")
+
+        self.music_params = MusicParams(master=self)
+
+
+class MusicParams(customtkinter.CTkFrame):
+
+    def __init__(self, *args, **kwargs):
+        super(MusicParams, self).__init__(*args, **kwargs)
+
+        
